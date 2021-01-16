6397c05aeacff85fcf4a99a869dcd9a426772a57
Gordon Lin <contact@gordonlin.ca>
2021-01-16 00:21:32 -0500

    Updated scripts

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
@@ -12,4 +11,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log("server is up!");
+});
