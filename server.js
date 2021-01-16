cd05f66b51018fbc46f987cdf64c7c378571bde9
Gordon Lin <contact@gordonlin.ca>
2021-01-16 13:00:59 -0500

    Added API middleware, setting up MSAL and Slack API

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
+const api = require("./routes/api.js");
+const msalAuth = require("./routes/msalAuth.js");
+const session = require('express-session');
+
 const app = express();
+
+app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'secret'
+}));
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+app.use("/api", api);
+app.use("/msal", msalAuth);
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
