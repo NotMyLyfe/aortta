14c994772a598371754d39baf2731d59f92c9a61
LNY <nixiaotang2004@gmail.com>
2021-01-17 03:48:57 -0500

    Added authentication

--- routes/api/slackApi.js
+++ routes/api/slackApi.js
@@ -1,18 +1,62 @@
 require('dotenv').config();
 const express = require('express');
 
+const { WebClient } = require('@slack/web-api');
+const SLACK_OAUTH_TOKEN = 'xoxb-1628208345143-1641047531589-QhmWl3Lud5UlHTbIeRsS25lP';
+const web = new WebClient(SLACK_OAUTH_TOKEN);
+
 const router = express.Router();
 
 router.post('/userToken', async (req, res, next) => {
 
-  //await function(req.body.item, req.body.item2).then()
+  var accessToken = req.body[0].access_token;
+  //console.log(accessToken);
 
-  console.log(req.body);
 
-  next(new Error("bitch things are messed up"));
+  web.chat.postMessage("general", "Hello");
+
+  if(accessToken != undefined) {
+      await sendMessageT(req.body[0].access_token, "general", "I have allowed Aortta to control me")
+        .then(response => {console.log(accessToken)})
+        .catch(err => {next(err);});
+  
+  }
+
+  res.sendStatus(200);
+
+})
+
+router.post('/sendMessage', async (req, res, next) => {
+
+  //{ user_token, channel, message } 
+
+  var r = req.body;
+
+  if(r.user_token != undefined && r.channel != undefined && r.message != undefined) {
+    await sendMessageT(r.user_token, r.channel, r.message)
+      .then(response => {
+        console.log(r.user_token);
+        console.log(r.channel);
+        console.log(r.message);
+      
+      })
+      .catch(err => {next(err);});
+
+  }
+
+  res.sendStatus(200);
+
 })
 
 
+async function sendMessageT(token, channel, message) {
 
+  web.chat.postMessage({
+      token: token,
+      channel: channel,
+      text: message,
+  })
+
+}
 
 module.exports = router;
--- slack/app.js
+++ slack/app.js
@@ -1,47 +1,14 @@
 
 require('dotenv').config();
 
-//const { createServer } = require('http');
-//const { InstallProvider } = require('@slack/oauth');
 const { RTMClient } = require('@slack/rtm-api');
 const { WebClient } = require('@slack/web-api');
 
 const SLACK_OAUTH_TOKEN = 'xoxb-1628208345143-1641047531589-QhmWl3Lud5UlHTbIeRsS25lP';
-//const CLIENT_ID = "1628208345143.1667973409392";
-//const CLIENT_SECRET = "10073a38ef2f45f8d52b24da0f306a75";
 
-//onst USER_TOKEN = "xoxp-1628208345143-1649438806708-1656764936657-9016c28cdb0529c152b601e1bfbea6c1"
 
 const rtm = new RTMClient(SLACK_OAUTH_TOKEN);
 const web = new WebClient(SLACK_OAUTH_TOKEN);
-
-
-/*
-// initialize the installProvider
-const installer = new InstallProvider({
-  clientId: CLIENT_ID,
-  clientSecret: CLIENT_SECRET,
-  stateSecret: 'my-state-secret',
-  authVersion: 'v1' //required for classic Slack apps
-});
-
-installer.generateInstallUrl({
-    // Add the scopes your app needs
-    scopes: ['channels:read'],
-    redirectUri: '/slack/oauth_redirect'
-})
-
-
-
-const server = createServer((req, res) =>  {
-  // our redirect_uri is /slack/oauth_redirect
-  if (req.url === '/slack/oauth_redirect') {
-    // call installer.handleCallback to wrap up the install flow
-    installer.handleCallback(req, res);
-  }
-})
-
-server.listen(3000);*/
 
 
 
@@ -112,12 +79,6 @@ async function sendMessage(channel, message) {
     })
 }
 
-async function sendMessageT(token, channel, message) {
-    await web.chat.postMessage({
-        token: token,
-        channel: channel,
-        text: message,
-    })
-}
+
 
 
