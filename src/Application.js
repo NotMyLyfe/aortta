f40b0e099aeb5cdf38629a606aac0bd502e909aa
draeve <yuxi@yuxiqin.ca>
2021-01-17 08:33:21 -0500

    added in application styles and input

--- src/Application.js
+++ src/Application.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import './application.css';
 
 
 function Application(props){
@@ -11,20 +12,19 @@ function Application(props){
     }
 
     return (
-        <div className="col-lg-6 col-md-12">
-            <div className="row">
+        <div id="wrapper-app" className="col-md-4 col-md-12">
+            <div id="inner-app" className="row">
                 <div className="col-md-2">
                     <ul className="nav nav-pills nav-pills-icons flex-column" role="tablist">
-
                         <li className="nav-item">
-                            <a className="nav-link" href="#dashboard-2" role="tab" data-toggle="tab"
+                            <a className="nav-link" href="#teams" role="tab" data-toggle="tab"
                                aria-selected="false">
                                 <i className="material-icons">dashboard</i>
                                 microsoft teams
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active show" href="#schedule-2" role="tab" data-toggle="tab"
+                            <a className="nav-link active show" href="#slack" role="tab" data-toggle="tab"
                                aria-selected="true">
                                 <i className="fas fa-user-friends"/>
                                 slack
@@ -32,13 +32,29 @@ function Application(props){
                         </li>
                     </ul>
                 </div>
-                <div className="col-md-8">
+
+                <div id="messaging-panel" className="col-md-8">
                     <div className="tab-content">
+                        <div className="tab-pane" id="teams">
+                            Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate
+                            B2C users after installed base benefits.
+                            <br/>
+                            Dramatically visualize customer directed convergence without revolutionary ROI.
+                            <input id="input-teams" type="text" className="form-control" placeholder="Send a message..."/>
+                        </div>
+
+                        <div className="tab-pane active show" id="slack">
+                            Efficiently unleash cross-media information without cross-media value. Quickly maximize timely
+                            deliverables for real-time schemas.
+                            <br/>Dramatically maintain clicks-and-mortar solutions without functional solutions.
+                            <input id="input-slack" type="text" className="form-control" placeholder="Send a message..."/>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
+
 
 export default Application;
