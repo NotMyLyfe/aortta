9da6a3abaefa7e5960082a81635bb8c82e42bcf5
draeve <yuxi@yuxiqin.ca>
2021-01-17 10:08:44 -0500

    changed auth tab to _blank

--- src/Login.js
+++ src/Login.js
@@ -55,10 +55,10 @@ function Login(props) {
         <div className="login">
             <div className="login-header">
                 <div className="card-header card-header-primary text-center login-card-header">
-                    <a href="https://slack.com/oauth/authorize?client_id=1628208345143.1667973409392&scope=bot,channels:history,channels:read,chat:write:bot,chat:write:user,files:read,files:write:user,im:history,im:read,im:write,incoming-webhook,links:read,mpim:history,mpim:read,usergroups:read,usergroups:write,users.profile:read,users.profile:write,users:read,users:read.email,users:write" className="btn btn-just-icon btn-link btn-white">
+                    <a target="_blank" href="https://slack.com/oauth/authorize?client_id=1628208345143.1667973409392&scope=bot,channels:history,channels:read,chat:write:bot,chat:write:user,files:read,files:write:user,im:history,im:read,im:write,incoming-webhook,links:read,mpim:history,mpim:read,usergroups:read,usergroups:write,users.profile:read,users.profile:write,users:read,users:read.email,users:write" className="btn btn-just-icon btn-link btn-white">
                         <i className="fab fa-slack"/>
                     </a>
-                    <a href="https://teams.microsoft.com/go#" className="btn btn-just-icon btn-link btn-white">
+                    <a target="_blank" href="https://teams.microsoft.com/go#" className="btn btn-just-icon btn-link btn-white">
                         <i className="fas fa-user-friends"/>
                         <div className="ripple-container"/>
                     </a>
