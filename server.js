bd23ca2a1d454fe9f378747c72fff636b6de0ed4
sa35577 <38817928+sa35577@users.noreply.github.com>
2021-01-17 03:35:47 -0500

    Vonage stuff

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(session({
     secret: 'secret'
 }));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use("/api", api);
 
